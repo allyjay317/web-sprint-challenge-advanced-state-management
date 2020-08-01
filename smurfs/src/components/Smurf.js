9d100e2ea8ea07cb593eef331cfbbe6ffa2dc941
Allison Usher <allyjay317@gmail.com>
2020-07-31 20:09:56 -0700

    added all data to smurf component

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Smurf = (props) => {
   return (
-    <div>
-      {props.data.name}
-    </div>
+    <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
+      <h2 style={{ width: '200px', textAlign: 'left' }}>{props.data.name}</h2>
+      <p style={{ width: '100px', textAlign: 'center' }}>{props.data.age}</p>
+      <p style={{ width: '200px', textAlign: 'center' }}>{props.data.height}</p>
+    </div >
   )
 }
 
